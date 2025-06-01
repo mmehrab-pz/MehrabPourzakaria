bd7759e447f20ffdf4987ab5f4db0820ab910a20
Mehrab <mmehrab.pk@gmail.com>
2025-06-01 20:47:13 +0330

    update(content)

--- asset/js/master.js
+++ asset/js/master.js
@@ -1,7 +1,15 @@
 const _li = document.querySelectorAll('#sideBar-navBar>ul>li')
 const _backs = document.querySelectorAll('#backs>div')
+const contents = document.querySelectorAll('#content>section')
+
 let _body = document.body
 let homeBtn = document.getElementById('home-btn')
+let home = document.getElementById('home')
+let about = document.getElementById('about')
+let portfolio = document.getElementById('portfolio')
+let services = document.getElementById('services')
+let contact = document.getElementById('contact')
+
 
 _li.forEach((item) => {
     item.addEventListener('click', () => {
@@ -9,38 +17,44 @@ _li.forEach((item) => {
             _li[i].classList.remove('navActive')
         }
         item.classList.add('navActive')
-        let idColor = item.getAttribute('data-idColor')
-        console.log(idColor);
+        let id = item.getAttribute('data-id')
 
         for (let i = 0; i < _backs.length; i++) {
-            _backs[i].classList.add('deactivateBack')
-            _backs[i].classList.remove('activeBack')
+            _backs[i].classList.add('deactivate')
+            _backs[i].classList.remove('active')
         }
+
+        contents.forEach(el => el.classList.remove('active'))
 
         _body.removeAttribute('class')
 
-        switch (idColor) {
+        switch (id) {
             case 'about':
-                document.getElementById('_aboutBack').classList.remove('deactivateBack')
-                document.getElementById('_aboutBack').classList.add('activeBack')
+                document.getElementById('_aboutBack').classList.remove('deactivate')
+                document.getElementById('_aboutBack').classList.add('active')
                 _body.classList.add('aboutBack')
+                about.classList.add('active')
+
                 break;
             case 'portfolio':
-                document.getElementById('_portfolioBack').classList.remove('deactivateBack')
-                document.getElementById('_portfolioBack').classList.add('activeBack')
+                document.getElementById('_portfolioBack').classList.remove('deactivate')
+                document.getElementById('_portfolioBack').classList.add('active')
                 _body.classList.add('portfolioBack')
+                portfolio.classList.add('active')
 
                 break;
             case 'services':
-                document.getElementById('_servicesBack').classList.remove('deactivateBack')
-                document.getElementById('_servicesBack').classList.add('activeBack')
+                document.getElementById('_servicesBack').classList.remove('deactivate')
+                document.getElementById('_servicesBack').classList.add('active')
                 _body.classList.add('servicesBack')
+                services.classList.add('active')
 
                 break;
             case 'contact':
-                document.getElementById('_contactBack').classList.remove('deactivateBack')
-                document.getElementById('_contactBack').classList.add('activeBack')
+                document.getElementById('_contactBack').classList.remove('deactivate')
+                document.getElementById('_contactBack').classList.add('active')
                 _body.classList.add('contactBack')
+                contact.classList.add('active')
 
                 break;
 
@@ -49,11 +63,17 @@ _li.forEach((item) => {
 });
 
 homeBtn.addEventListener('click', () => {
+    _li.forEach(el => el.classList.remove('navActive'))
+    
     for (let i = 0; i < _backs.length; i++) {
-        _backs[i].classList.add('deactivateBack')
-        _backs[i].classList.remove('activeBack')
+        _backs[i].classList.add('deactivate')
+        _backs[i].classList.remove('active')
     }
-    document.getElementById('_homeBack').classList.remove('deactivateBack')
-    document.getElementById('_homeBack').classList.add('activeBack')
+
+    contents.forEach(el => el.classList.remove('active'))
+
+    document.getElementById('_homeBack').classList.remove('deactivate')
+    document.getElementById('_homeBack').classList.add('active')
     _body.classList.add('homeBack')
+    home.classList.add('active')
 })
